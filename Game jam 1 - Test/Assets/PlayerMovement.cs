bdeaffa1e0312a2f0911668252dbed8333126ba0
AstralWz14 <129330549+AstralWz14@users.noreply.github.com>
2023-04-04 19:42:54 +0100

    Character movement complete

--- Game jam 1 - Test/Assets/PlayerMovement.cs
+++ Game jam 1 - Test/Assets/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
@@ -17,11 +19,26 @@ public class PlayerMovement : MonoBehaviour
         
         horizontalMove = Input.GetAxisRaw("Horizontal") *runSpeed;
 
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+    }
+
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
-    }
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }   
 
 }
